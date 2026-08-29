42b30fb87ee2ef7fa864bfd67bfef0fdde18aa71
prathamestates <prathamparmar849@gmail.com>
2026-08-29 03:09:57 +0100

    Initialize main.jsx with React app structure

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StocklyApp from "./App.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <StocklyApp />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
